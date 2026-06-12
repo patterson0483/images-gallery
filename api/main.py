b1e7fb784c91579c0ba20e1c1c70decc3950ddca
Erica Wynn <pattmd25@yahoo.com>
2026-06-12 13:16:58 -0400

    mongo stuff

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ import requests
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+# from mongo_client import insert_test_document
 
 load_dotenv(".env.local")
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
@@ -11,6 +12,7 @@ UNSPLASH_KEY = os.getenv("UNSPLASH_KEY")
 
 app = Flask(__name__)
 CORS(app)
+# insert_test_document()
 
 @app.route('/new-image')
 def new_image():
